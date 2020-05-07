9ca6564660c4b3d25e247d182aef70cc8c0179d2
Julani7 <53162801+Julani7@users.noreply.github.com>
2020-05-07 15:47:08 -0400

    book app

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -98,7 +98,6 @@ const render = function () {
     renderError();
 
     let items = store.filterList(store.filter);
-    console.log(store.filter)
     let html = generateMainMenu();
 
     if (store.adding === false) {
